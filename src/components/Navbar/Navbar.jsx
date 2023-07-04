23be78300e1440431e85c708d5f0fb7b2be3103e
NguyenSanh <jsn006@ucsd.edu>
2023-07-04 00:10:22 -0700

    Worked on SmallScreen Div

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -29,6 +29,16 @@ const Navbar = () => (
     </div>
     <div className='app__navbar-smallscreen'>
       <GiHamburgerMenu color= "#FFF " fontSize={27} onClick={() => {}} />
+      <div className='app__navbar-smallscreen_overlay flex-center slide-bottom'>
+        <MdOutlineRestaurantMenu fontSize={27} className='overlay_close' onClick={() => {}}/>
+        <ul className='app__navbar-smallscreen-links'>
+          <li className="p__opensans"> <a href="#home">Home</a></li>
+          <li className="p__opensans"> <a href="#about">About</a></li>
+          <li className="p__opensans"> <a href="#menu">Menu</a></li>
+          <li className="p__opensans"> <a href="#awards">Awards</a></li>
+          <li className="p__opensans"> <a href="#contact">Contact</a></li>
+        </ul>
+      </div>
     </div>
   </div>
 );
