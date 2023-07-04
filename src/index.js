5c853151dca915190f22515425370cd5ea000a48
NguyenSanh <jsn006@ucsd.edu>
2023-07-03 17:54:46 -0700

    Added starter_src folder

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-/* The index.js file is the most important file */
-/* This is where we connect our React app to the DOM */
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
