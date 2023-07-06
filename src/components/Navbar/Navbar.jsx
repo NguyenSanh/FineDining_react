6d4f9dc09f5904ba2ec670207c122ee94a12c508
NguyenSanh <jsn006@ucsd.edu>
2023-07-05 19:38:35 -0700

    Started About Us Section
    Added About Us Header, spoon image, and button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 /* Import simple hamburger button from React library */
 import { GiHamburgerMenu } from 'react-icons/gi';
@@ -10,37 +10,46 @@ import images from '../../constants/images'; // '../' is used to move up a direc
 /* Import the CSS file used to style Navbar.jsx */
 import './Navbar.css';
 
-const Navbar = () => (
-  <div className="app__navbar">
-    <div className='app__navbar-logo'>
-      <img src={images.gericht} alt="app logo" />
-    </div>
-    <ul className='app__navbar-links'>
-      <li className="p__opensans"> <a href="#home">Home</a></li>
-      <li className="p__opensans"> <a href="#about">About</a></li>
-      <li className="p__opensans"> <a href="#menu">Menu</a></li>
-      <li className="p__opensans"> <a href="#awards">Awards</a></li>
-      <li className="p__opensans"> <a href="#contact">Contact</a></li>
-    </ul>
-    <div className="app__navbar-login">
-      <a href="#login" className="p__opensans">Log In / Register</a>
-      <div/>
-      <a href="/" className='p__opensans'> Book Table </a>
-    </div>
-    <div className='app__navbar-smallscreen'>
-      <GiHamburgerMenu color= "#FFF " fontSize={27} onClick={() => {}} />
-      <div className='app__navbar-smallscreen_overlay flex-center slide-bottom'>
-        <MdOutlineRestaurantMenu fontSize={27} className='overlay_close' onClick={() => {}}/>
-        <ul className='app__navbar-smallscreen-links'>
-          <li className="p__opensans"> <a href="#home">Home</a></li>
-          <li className="p__opensans"> <a href="#about">About</a></li>
-          <li className="p__opensans"> <a href="#menu">Menu</a></li>
-          <li className="p__opensans"> <a href="#awards">Awards</a></li>
-          <li className="p__opensans"> <a href="#contact">Contact</a></li>
-        </ul>
+const Navbar = () => {
+  /* Set the toggle functionality for the GiHamburgerMenu here: */
+  const [toggleMenu, setToggleMenu] = React.useState(false); // useState is used for dynamic changes
+
+  return (
+    <div className="app__navbar">
+      <div className='app__navbar-logo'>
+        <img src={images.gericht} alt="app logo" />
+      </div>
+      <ul className='app__navbar-links'>
+        <li className="p__opensans"> <a href="#home">Home</a></li>
+        <li className="p__opensans"> <a href="#about">About</a></li>
+        <li className="p__opensans"> <a href="#menu">Menu</a></li>
+        <li className="p__opensans"> <a href="#awards">Awards</a></li>
+        <li className="p__opensans"> <a href="#contact">Contact</a></li>
+      </ul>
+      <div className="app__navbar-login">
+        <a href="#login" className="p__opensans">Log In / Register</a>
+        <div/>
+        <a href="/" className='p__opensans'> Book Table </a>
+      </div>
+      <div className='app__navbar-smallscreen'>
+        <GiHamburgerMenu color= "#FFF " fontSize={27} onClick={() => setToggleMenu(true)} />
+
+        {toggleMenu && (
+          <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
+            <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={() => setToggleMenu(false)}/>
+            <ul className='app__navbar-smallscreen_links'>
+              <li className="p__opensans"> <a href="#home">Home</a></li>
+              <li className="p__opensans"> <a href="#about">About</a></li>
+              <li className="p__opensans"> <a href="#menu">Menu</a></li>
+              <li className="p__opensans"> <a href="#awards">Awards</a></li>
+              <li className="p__opensans"> <a href="#contact">Contact</a></li>
+            </ul>
+          </div>
+        )}
+
       </div>
     </div>
-  </div>
-);
+  )
+};
 
 export default Navbar;
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
+import { images } from '../../constants'; // Import image.js file
 import './Header.css';
+import { SubHeading } from '../../components'; // Import SubHeading component
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id="home">
+    <div className='app__wrapper_info'>
+      <SubHeading title='Maruchan Instant Noodles'/>
+      <h1 className='app__header-h1'>The Key to Fine Cancer</h1>
+      <p className='p__opensans' style = {{ margin: '2rem 0' }}>Maruchan noodles will save you plenty of cash!</p>
+      <button type="button" className='custom__button'>Explore our menu of noodles!</button>
+    </div>
+    <div className='app__wrapper_img'>
+      <img src={images.welcome} alt="header image" />
+        
+    </div>
   </div>
 );
 
